cde4ad68f13c484219c3f8b03f74f0220a333ccf
gaganchaudary4421-ops <gaganchaudhary4421@gmail.com>
2026-06-13 15:22:10 +0530

    add live url

--- backend/main.py
+++ backend/main.py
@@ -39,7 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",    
         "http://localhost:3000",  
-        "https://yourdomain.com",   
+        "https://neural-path-gamma.vercel.app/",   
     ],
     allow_credentials=True,
     allow_methods=["*"],
